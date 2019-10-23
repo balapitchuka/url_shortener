339ecc2fc52687247c345180f5601965bf98185a
Balasubrahmanyam <pbs@gmail.com>
2019-10-23 17:03:00 +0530

    'urlshortner-node-implementation

---
+++ src/server.js
@@ -0,0 +1,85 @@
+require('dotenv').config();
+
+const path = require('path')
+const express = require('express');
+const bodyParser = require('body-parser');
+const dns = require('dns');
+const nanoid = require('nanoid');
+
+
+const { MongoClient } = require('mongodb');
+
+const databaseUrl = process.env.DATABASE;
+
+const app =  express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+MongoClient.connect(databaseUrl, {  useUnifiedTopology: true })
+  .then(client => {
+    app.locals.db = client.db('shortener');
+  })
+  .catch(() => console.error('Failed to connect to the database'));
+
+
+const shortenURL = (db, url) => {
+    const shortenedURLs = db.collection('shortenedURLs');
+    return shortenedURLs.findOneAndUpdate({ original_url: url },
+      {
+        $setOnInsert: {
+          original_url: url,
+          short_id: nanoid(7),
+        },
+      },
+      {
+        returnOriginal: false,
+        upsert: true,
+      }
+    );
+  };
+  
+
+
+
+
+app.get('/', (req, res) => {
+    const htmlPath = path.join(__dirname, 'public', 'index.html')
+    res.sendFile(htmlPath);
+})
+
+
+app.post('/new', (req, res) => {
+    let originalUrl;
+  try {
+    originalUrl = new URL(req.body.url);
+  } catch (err) {
+    return res.status(400).send({ error: 'invalid URL' });
+  }
+
+  dns.lookup(originalUrl.hostname, (err) => {
+    if (err) {
+      return res.status(404).send({ error: 'Address not found' });
+    };
+
+    const { db } = req.app.locals;
+    shortenURL(db, originalUrl.href)
+      .then(result => {
+        const doc = result.value;
+        res.json({
+          original_url: doc.original_url,
+          short_id: doc.short_id,
+        });
+      })
+      .catch(console.error);
+  
+  });
+
+
+});
+
+app.set('port', process.env.PORT || 4600);
+
+const server = app.listen(app.get('port'), () => {
+    console.log(`Express server running on PORT ${server.address().port}`);
+});
